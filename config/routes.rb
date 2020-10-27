bfb54aec1941bee6406725393b36d2bb7354feaf
GarethWoodman <gareth.woodman92@gmail.com>
2020-10-27 18:17:06 +0000

    Added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'user/new'
+  get 'sessions/new'
+  get 'sessions/create'
+  #get url and point to controller#method
+  get '/', to: 'user#new'
   get 'user/create'
   get 'hello_world/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- spec/views/user/user_can_register_spec.rb
+++ spec/views/user/user_can_register_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "User account register", type: :view do
   describe 'a user can register with an account' do
-    user = User.create(fname: 'Gareth', lname: 'Woodman', email: 'gareth.woodman@gmail.com', password: 'password')
-
+    #user = User.create(username: "gareth2020", email: 'gareth.woodman92@gmail.com', password: 'password')
     visit '/'
-
     expect(page).to have_content 'Register'
+
+    fill_in 'user[username]', with: 'gareth2020'
+    fill_in 'user[email]', with: 'gareth.woodman92@gmail.com'
+    fill_in 'user[password]', with: 'password'
+    fill_in 'user[password_confirmation]', with: 'password'
+    find('[name=commit]').click
+
+    expect(current_path).to eq '/sessions/new'
   end
 end
