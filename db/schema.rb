10e5a21a3aa1c69f8738c0734a09141c301bede8
GarethWoodman <gareth.woodman92@gmail.com>
2020-10-27 16:48:44 +0000

    Switched to PostgreSQL as default DB

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_10_27_161809) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
